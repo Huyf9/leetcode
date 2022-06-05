61ef0e7fc5b0dcb297f870cb0cc0d8e8d33a2262
Huyf9 <98331980+Huyf9@users.noreply.github.com>
2022-06-05 20:04:17 +0800

    Create main.cpp

---
+++ leetcode35_搜索插入位置/main.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l = 0, r = nums.size() - 1;
+        int mid = 0;
+        while (l<=r){
+            mid = l + (r - l) / 2;
+            if (nums[mid] < target) l = mid + 1;
+            else r = mid - 1;
+        }
+        return l + (r - l) / 2;
+    }
+};
