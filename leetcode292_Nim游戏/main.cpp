3a2bfa2d9c3ea42dc5938ad5e376590c2364fa22
Huyf9 <98331980+Huyf9@users.noreply.github.com>
2022-06-06 16:23:02 +0800

    Create main.cpp

---
+++ leetcode292_Nim游戏/main.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+
+class Solution {
+public:
+    bool canWinNim(int n) {
+        if(n<4) return true;
+        else
+            if(n%4==0) return false;
+            else return true;
+    }
+};
